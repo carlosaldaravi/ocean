877c9feba60717f17366f08f30ce5f19104956e5
Carlos Aldaravi <carlosaldaravi11@gmail.com>
2020-02-01 11:49:31 +0100

    Configurando tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <button class="btn">
+      aceptar
+    </button>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
