2b9baa361767711fcbb96016b7656e687ee71aee
Carlos <carlosaldaravi@gmail.com>
2020-05-07 04:43:02 +0200

    [FIX] backend route

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const config = {
-    headers: {
-        "Authorization" : localStorage.token
-    },
-    apiURL: 'http://localhost:3000',
-    network:{
-        SUCCESS: 200,
-        UNAUTHORIZED: 401
-    }
-}
+  headers: {
+    Authorization: localStorage.token,
+  },
+  apiURL: "http://localhost:3000/api/",
+  network: {
+    SUCCESS: 200,
+    UNAUTHORIZED: 401,
+  },
+};
