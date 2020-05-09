89df246e0eba223dd967294e4967dd3591c986a5
Carlos <carlosaldaravi@gmail.com>
2020-05-09 23:09:09 +0200

    [UP] main prettier

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
 
 // CSS
-import './assets/css/output.css'
+import "./assets/css/output.css";
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+const token = localStorage.getItem("user-token");
+if (token) {
+  axios.defaults.headers.common["Authorization"] = token;
+}
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
