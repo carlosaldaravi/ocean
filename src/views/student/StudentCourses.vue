1d132fd8b4e826c0b0559791409d413608df94a8
Carlos <carlosaldaravi@gmail.com>
2020-05-12 01:07:16 +0200

    [UP] student index and courses

---
+++ src/views/student/StudentCourses.vue
@@ -0,0 +1,87 @@
+<template>
+  <section class>
+    <section class="container p-4 md:p-8 mx-auto">
+      <h1
+        class="text-3xl text-center leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
+      >
+        Mis cursos
+      </h1>
+      <div>
+        <div class="mb-4">
+          <nav class="flex justify-center">
+            <a
+              :class="{ 'bg-green-400 text-white': tab == 'past' }"
+              href="#"
+              @click="tab = 'past'"
+              class="px-3 py-2 font-medium text-sm leading-5 rounded-md text-gray-600 hover:text-gray-800"
+              >Realizados</a
+            >
+            <a
+              @click="tab = 'next'"
+              :class="{ 'bg-green-400 text-white': tab == 'next' }"
+              href="#"
+              class="ml-4 px-3 py-2 font-medium text-sm leading-5 rounded-md text-gray-600 hover:text-gray-800"
+              >Próximos</a
+            >
+          </nav>
+        </div>
+        <CourseList :courses="coursesList"></CourseList>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+import CourseList from "../../components/CourseList.vue";
+import { Course } from "../../classes/course";
+import { API } from "../../classes/api";
+export default {
+  data() {
+    return {
+      api: new API(),
+      courses: [],
+      // values: 'next', 'past'
+      tab: "next",
+    };
+  },
+  components: {
+    CourseList,
+  },
+  created() {
+    this.getCourses();
+  },
+  computed: {
+    coursesList: function() {
+      return this.tab == "next" ? this.nextCourses : this.pastCourses;
+    },
+    nextCourses: function() {
+      return this.courses.filter(
+        (course) =>
+          new Date() <
+          new Date(
+            `${course.calendar[0].date} ${course.calendar[0].start_time}`
+          )
+      );
+    },
+    pastCourses: function() {
+      return this.courses.filter(
+        (course) =>
+          new Date() >
+          new Date(
+            `${course.calendar[0].date} ${course.calendar[0].start_time}`
+          )
+      );
+    },
+  },
+  methods: {
+    async getCourses() {
+      let res = await this.api.get("students/courses");
+      res.data.data.forEach((course) => {
+        this.courses.push(new Course(course));
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/student/StudentIndex.vue
+++ src/views/student/StudentIndex.vue
@@ -1,13 +1,69 @@
 <template>
-<div>Index del alumno</div>
+  <div>
+    <Menu></Menu>
+    <div>
+      <h3 class="text-lg leading-6 font-medium text-gray-900">Dashboard</h3>
+      <div @click="goToCourses()" class="mt-5 grid grid-cols-2 gap-5 sm:grid-cols-3 lg:grid-cols-4">
+        <div class="item bg-white overflow-hidden shadow rounded-lg">
+          <div class="px-4 py-5 sm:p-6">
+            <div class="flex items-center">
+              <div class="flex-shrink-0 bg-indigo-500 rounded-md p-3">
+                <svg
+                  class="h-6 w-6 text-white"
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+              </div>
+              <div class="ml-5 w-0 flex-1">
+                <dl>
+                  <dt class="text-sm leading-5 font-medium text-gray-500 truncate"></dt>
+                  <dd class="flex items-baseline">
+                    <div class="text-2xl leading-8 font-semibold text-gray-900">Mis clases</div>
+                  </dd>
+                </dl>
+              </div>
+            </div>
+          </div>
+          <div class="bg-gray-50 px-4 py-4 sm:px-6">
+            <div class="text-sm leading-5">
+              <a
+                href="#"
+                class="font-medium text-indigo-600 hover:text-indigo-500 transition ease-in-out duration-150"
+              >View all</a>
+            </div>
+          </div>
+        </div>
+        <!-- end card -->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Menu from "../../components/layout/Menu";
 export default {
-
-}
+  methods: {
+    goToCourses() {
+      this.$router.push(`/mis-cursos`);
+    }
+  },
+  components: {
+    Menu
+  }
+};
 </script>
 
-<style>
-
+<style scooped>
+.item {
+  background-color: #928b9c;
+  background-image: url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 31c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM12 86c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm28-65c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm23-11c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-6 60c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm29 22c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zM32 63c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm57-13c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9-21c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM60 91c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM35 41c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM12 60c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z' fill='%239C92AC' fill-opacity='0.39' fill-rule='evenodd'/%3E%3C/svg%3E");
+}
 </style>
