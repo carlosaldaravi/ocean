8cc3dda45a17f73f98d5c96df332438630e7bce2
Carlos <carlosaldaravi@gmail.com>
2020-05-12 20:50:47 +0200

    [UP] student index

--- src/components/student/StudentIndex.vue
+++ src/components/student/StudentIndex.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="flex justify-between mx-4">
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-32">
     <div
       @click="goToCourses()"
-      class="w-36 h-36 bg-gray-200 rounded-lg shadow-2xl relative overflow-hidden"
+      class="h-28 bg-gray-200 sm:h-56 rounded-lg shadow-2xl relative overflow-hidden"
     >
-      Cursos
+      <img src="../../assets/images/index_mis_clases.png" alt="" />
     </div>
     <div
-      class="w-36 h-36 bg-gray-200 rounded-lg shadow-2xl relative overflow-hidden"
-    ></div>
+      @click="goToCalendar()"
+      class="h-28 bg-gray-200 sm:h-56 rounded-lg shadow-2xl relative overflow-hidden"
+    >
+      <img src="../../assets/images/index_calendario.png" alt="" />
+    </div>
+    <div
+      @click="goToTargets()"
+      class="h-28 bg-gray-200 sm:h-56 rounded-lg shadow-2xl relative overflow-hidden"
+    >
+      <img src="../../assets/images/index_objetivos.png" alt="" />
+    </div>
+    <div
+      @click="goToProfile()"
+      class="h-28 bg-gray-200 sm:h-56 rounded-lg shadow-2xl relative overflow-hidden"
+    >
+      <img src="../../assets/images/perfil.png" alt="" />
+    </div>
   </div>
 </template>
 
@@ -18,6 +33,15 @@ export default {
   methods: {
     goToCourses() {
       this.$router.push(`/cursos`);
+    },
+    goToCalendar() {
+      this.$router.push(`/calendar`);
+    },
+    goToTargets() {
+      this.$router.push(`/targets`);
+    },
+    goToProfile() {
+      this.$router.push(`/profile`);
     },
   },
   components: {
