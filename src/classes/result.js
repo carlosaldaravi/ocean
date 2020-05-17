008311245f1d055b13630112a17f72ef7e7a649f
Carlos <carlosaldaravi@gmail.com>
2020-05-17 05:53:33 +0200

    [REF] code spacing

--- src/classes/result.js
+++ src/classes/result.js
@@ -1,21 +1,20 @@
-
 // This is an axios wrapper. Only Get and Post methods are defined here since we dont use Put, Patch and Delete calls.
 // There are try-catch blocks so we can easily use the Vue Alert component when a request fails
 export const Result = class Result {
-    constructor(url = null) {
-        this.success = null;
-        this.data = null;
-    }
+  constructor(url = null) {
+    this.success = null;
+    this.data = null;
+  }
 
-    error(data) {
-        this.data = data;
-        this.success = false;
-        return {data: this.data, success: this.success};
-    }
+  error(data) {
+    this.data = data;
+    this.success = false;
+    return { data: this.data, success: this.success };
+  }
 
-    ok(data) {
-        this.data = data;
-        this.success = true;
-        return {data: this.data, success: this.success};
-    }
-}
+  ok(data) {
+    this.data = data;
+    this.success = true;
+    return { data: this.data, success: this.success };
+  }
+};
--- src/classes/student.js
+++ src/classes/student.js
@@ -1,11 +1,11 @@
 export const Student = class Student {
-    constructor(data = null) {
-        if(!data) return;
-        this.nombre = data.nombre;
-        this.apellido1 = data.apellido1;
-        this.apellido2 = data.apellido2;
-        this.email = data.email;
-        this.telefono = data.telefono;
-        this.imagen = data.picture.medium;
-    }
-}
+  constructor(data = null) {
+    if (!data) return;
+    this.nombre = data?.nombre;
+    this.apellido1 = data?.apellido1;
+    this.apellido2 = data?.apellido2;
+    this.email = data?.email;
+    this.telefono = data?.telefono;
+    this.imagen = data?.picture.medium;
+  }
+};
