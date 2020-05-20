aa3b905a6b067b0bcdb1510bca84791e5c90e635
Carlos <carlosaldaravi@gmail.com>
2020-05-21 00:49:57 +0200

    [FIX] target model class

--- src/classes/target.js
+++ src/classes/target.js
@@ -1,12 +1,25 @@
 export const Target = class Target {
   constructor(data = null) {
     this.target = {
-      id: data?.id,
-      name: data?.name,
-      levelId: data?.levelId,
-      description: data?.description,
-      sportId: data?.sportId,
+      id: data?.target?.id,
+      name: data?.target?.name,
+      levelId: data?.target?.levelId,
+      level: {
+        name: data?.target?.level?.name,
+        order: data?.target?.level?.order,
+      },
+      description: data?.target?.description,
+      sportId: data?.target?.sportId,
+    };
+    this.instructor = {
+      details: {
+        firstname: data?.instructor?.details?.firstname,
+        gender: data?.instructor?.details?.gender,
+      },
     };
     this.validatedBy = data?.validatedBy;
+    this.date = data?.date;
+    this.feedback = data?.feedback;
+    this.targetId = data?.targetId;
   }
 };
