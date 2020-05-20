d58fda727b6e5f31e62e5eecca596940bb5ffd1a
Carlos <carlosaldaravi@gmail.com>
2020-05-21 00:49:36 +0200

    [ADD] tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
     inset: {
       "1/2": "50%",
     },
+    extend: {
+      opacity: {
+        "10": "0.1",
+        "20": "0.2",
+        "95": "0.95",
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/ui")({
