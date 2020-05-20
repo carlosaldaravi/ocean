5072b15c72a8a06becc4ca770af54dd1d401c295
Carlos <carlosaldaravi@gmail.com>
2020-05-21 00:51:19 +0200

    [UP] code by plugin Headwind

--- src/components/course/CourseCard.vue
+++ src/components/course/CourseCard.vue
@@ -1,27 +1,27 @@
 <template>
   <div
-    class="flex flex-col sm:flex-row h-auto bg-gray-100 sm:h-56 rounded shadow relative overflow-hidden"
+    class="relative flex flex-col h-auto overflow-hidden bg-gray-100 bg-opacity-75 rounded shadow sm:flex-row bg-opa sm:h-56"
   >
     <div class="absolute right-0">
       <img class="w-8 h-8 rounded" src="../../assets/images/spain.png" alt />
     </div>
 
-    <div class="w-full sm:w-1/3 h-full">
+    <div class="w-full h-full sm:w-1/3">
       <img
         v-if="course.level.name == 'Iniciación'"
-        class="w-full h-40 sm:h-full object-cover"
+        class="object-cover w-full h-40 sm:h-full"
         src="../../assets/images/iniciacion.jpg"
         alt
       />
       <img
         v-else
-        class="w-full h-40 sm:h-full object-cover"
+        class="object-cover w-full h-40 sm:h-full"
         src="../../assets/images/kitesurf.png"
         alt
       />
     </div>
 
-    <div class="w-full sm:w-2/3 h-full p-4">
+    <div class="w-full h-full p-4 sm:w-2/3">
       <!-- hora fecha -->
       <div class="flex justify-between">
         <div class="flex items-center">
@@ -38,7 +38,7 @@
         </div>
       </div>
       <!-- nivel  -->
-      <div class="align-middle pt-2 flex">
+      <div class="flex pt-2 align-middle">
         <div class="flex items-center">
           <div class="text-3xl">{{ course.level.name }}</div>
         </div>
@@ -49,42 +49,34 @@
           <div>
             <img
               v-if="course.instructors[0].details.gender == 'FEMALE'"
-              class="w-9 h-9 object-cover object-top rounded-full"
+              class="object-cover object-top rounded-full w-9 h-9"
               src="../../assets/images/avatar/kaya.jpg"
               alt
             />
             <img
               v-if="course.instructors[0].details.gender == 'MALE'"
-              class="w-9 h-9 object-cover object-top rounded-full"
+              class="object-cover object-top rounded-full w-9 h-9"
               src="../../assets/images/avatar/will.jpg"
               alt
             />
           </div>
           <div class="ml-3">
             <p
-              class="text-sm leading-5 font-medium text-gray-700 group-hover:text-gray-900"
-            >
-              {{ course.instructors[0].details.firstname }}
-            </p>
+              class="text-sm font-medium leading-5 text-gray-700 group-hover:text-gray-900"
+            >{{ course.instructors[0].details.firstname }}</p>
             <p
               v-if="course.instructors[0].details.gender == 'FEMALE'"
-              class="text-xs leading-4 font-medium text-gray-500 group-hover:text-gray-700 group-focus:underline transition ease-in-out duration-150"
-            >
-              Instructora
-            </p>
+              class="text-xs font-medium leading-4 text-gray-500 transition duration-150 ease-in-out group-hover:text-gray-700 group-focus:underline"
+            >Instructora</p>
             <p
               v-else
-              class="text-xs leading-4 font-medium text-gray-500 group-hover:text-gray-700 group-focus:underline transition ease-in-out duration-150"
-            >
-              Instructor
-            </p>
+              class="text-xs font-medium leading-4 text-gray-500 transition duration-150 ease-in-out group-hover:text-gray-700 group-focus:underline"
+            >Instructor</p>
           </div>
         </div>
         <div class="flex items-center">
-          <img class="w-6 h-6 " src="../../assets/images/logros.svg" alt />
-          <div class=" pl-1">
-            8/10
-          </div>
+          <img class="w-6 h-6" src="../../assets/images/logros.svg" alt />
+          <div class="pl-1">8/10</div>
         </div>
       </div>
     </div>
@@ -96,7 +88,7 @@ import { formatDate, formatTime } from "../../helpers/functions";
 
 export default {
   props: {
-    course: { type: Object, required: true },
+    course: { type: Object, required: true }
   },
   methods: {
     formatDate,
@@ -107,8 +99,8 @@ export default {
       } else {
         return "instructor";
       }
-    },
-  },
+    }
+  }
 };
 </script>
 
