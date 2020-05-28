8a189fbb9d03a3ada2698ca1aae66f2868efad87
Carlos <carlosaldaravi@gmail.com>
2020-05-28 20:33:29 +0200

    [ADD] transformation scale to buttons

--- src/components/student/StudentIndex.vue
+++ src/components/student/StudentIndex.vue
@@ -2,25 +2,25 @@
   <div class="grid grid-cols-1 gap-1 md:grid-cols-2 md:gap-32">
     <div
       @click="goToCourses()"
-      class="relative flex items-center justify-center h-24 overflow-hidden text-center transition duration-300 ease-in-out bg-gray-100 bg-opacity-25 rounded-lg shadow-2xl cursor-pointer hover:bg-opacity-75 md:h-56"
+      class="relative flex items-center justify-center h-24 overflow-hidden text-center transition duration-300 ease-in-out transform bg-gray-100 bg-opacity-25 rounded-lg shadow-2xl cursor-pointer hover:scale-105 hover:bg-opacity-75 md:h-56"
     >
       <div class="absolute font-mono text-3xl font-black sm:text-4xl md:text-5xl">CLASES</div>
     </div>
     <div
       @click="goToCalendar()"
-      class="relative flex items-center justify-center h-24 overflow-hidden text-center transition duration-300 ease-in-out bg-gray-100 bg-opacity-25 rounded-lg shadow-2xl cursor-pointer hover:bg-opacity-75 md:h-56"
+      class="relative flex items-center justify-center h-24 overflow-hidden text-center transition duration-300 ease-in-out transform bg-gray-100 bg-opacity-25 rounded-lg shadow-2xl cursor-pointer hover:scale-105 hover:bg-opacity-75 md:h-56"
     >
       <div class="absolute font-mono text-3xl font-black sm:text-4xl md:text-5xl">CALENDARIO</div>
     </div>
     <div
       @click="goToTargets()"
-      class="relative flex items-center justify-center h-24 overflow-hidden text-center transition duration-300 ease-in-out bg-gray-100 bg-opacity-25 rounded-lg shadow-2xl cursor-pointer hover:bg-opacity-75 md:h-56"
+      class="relative flex items-center justify-center h-24 overflow-hidden text-center transition duration-300 ease-in-out transform bg-gray-100 bg-opacity-25 rounded-lg shadow-2xl cursor-pointer hover:scale-105 hover:bg-opacity-75 md:h-56"
     >
       <div class="absolute font-mono text-3xl font-black sm:text-4xl md:text-5xl">OBJETIVOS</div>
     </div>
     <div
       @click="goToProfile()"
-      class="relative flex items-center justify-center h-24 overflow-hidden text-center transition duration-300 ease-in-out bg-gray-100 bg-opacity-25 rounded-lg shadow-2xl cursor-pointer hover:bg-opacity-75 md:h-56"
+      class="relative flex items-center justify-center h-24 overflow-hidden text-center transition duration-300 ease-in-out transform bg-gray-100 bg-opacity-25 rounded-lg shadow-2xl cursor-pointer hover:scale-105 hover:bg-opacity-75 md:h-56"
     >
       <div class="absolute font-mono text-3xl font-black sm:text-4xl md:text-5xl">PERFIL</div>
     </div>
