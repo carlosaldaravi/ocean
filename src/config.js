f5e68039663d5422f435da14f9f87076eed9ceaa
Carlos <carlosaldaravi@gmail.com>
2020-07-09 12:15:35 +0200

    [UP] config to production

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ export const config = {
   headers: {
     Authorization: "Bearer " + localStorage.getItem("user-token"),
   },
-  apiURL: "http://localhost:3000/api/",
+  // apiURL: "http://localhost:3000/api/",
+  apiURL: "https://api-ocean.herokuapp.com/api/",
   network: {
     SUCCESS: 200,
     UNAUTHORIZED: 401,
