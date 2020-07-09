8839f2a45f37bcbb56974f8b72bb9ffb02d3f96a
Carlos <carlosaldaravi@gmail.com>
2020-07-09 09:09:28 +0200

    [CHG] student model

--- src/classes/student.js
+++ src/classes/student.js
@@ -1,11 +1,14 @@
 export const Student = class Student {
   constructor(data = null) {
     if (!data) return;
-    this.nombre = data?.nombre;
-    this.apellido1 = data?.apellido1;
-    this.apellido2 = data?.apellido2;
+    this.id = data?.id;
     this.email = data?.email;
-    this.telefono = data?.telefono;
-    this.imagen = data?.picture.medium;
+    this.details = {
+      firstname: data?.details?.firstname,
+      lastname: data?.details?.lastname,
+      phone: data?.details?.phone,
+      dateBorn: data?.details?.dateBorn,
+      city: data?.details?.city,
+    };
   }
 };
