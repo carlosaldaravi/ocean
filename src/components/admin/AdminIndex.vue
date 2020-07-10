365dd4cbcf6c9b0386dc716b0799cd0a398011e6
Carlos <carlosaldaravi@gmail.com>
2020-07-10 21:11:57 +0200

    [ADD] admin view home

---
+++ src/components/admin/AdminIndex.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="grid grid-cols-1 gap-1 md:grid-cols-2 md:gap-32">
+    <div
+      @click="goToCalendar()"
+      class="relative flex items-center justify-center h-24 overflow-hidden text-center transition duration-300 ease-in-out transform bg-gray-100 bg-opacity-25 rounded-lg shadow-2xl cursor-pointer hover:scale-105 hover:bg-opacity-75 md:h-56"
+    >
+      <div
+        class="absolute font-mono text-3xl font-black sm:text-4xl md:text-5xl"
+      >
+        CALENDARIO
+      </div>
+    </div>
+    <div
+      @click="goToStatistics()"
+      class="relative flex items-center justify-center h-24 overflow-hidden text-center transition duration-300 ease-in-out transform bg-gray-100 bg-opacity-25 rounded-lg shadow-2xl cursor-pointer hover:scale-105 hover:bg-opacity-75 md:h-56"
+    >
+      <div
+        class="absolute font-mono text-3xl font-black sm:text-4xl md:text-5xl"
+      >
+        ESTADÍSTICAS
+      </div>
+    </div>
+    <div
+      @click="goToProfile()"
+      class="relative flex items-center justify-center h-24 overflow-hidden text-center transition duration-300 ease-in-out transform bg-gray-100 bg-opacity-25 rounded-lg shadow-2xl cursor-pointer hover:scale-105 hover:bg-opacity-75 md:h-56"
+    >
+      <div
+        class="absolute font-mono text-3xl font-black sm:text-4xl md:text-5xl"
+      >
+        PERFIL
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Menu from "../../components/layout/Menu";
+export default {
+  methods: {
+    goToCalendar() {
+      this.$router.push(`/calendar`);
+    },
+    goToStatistics() {
+      this.$router.push(`/statistics`);
+    },
+    goToProfile() {
+      this.$router.push(`/profile`);
+    },
+  },
+  components: {
+    Menu,
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="container p-4 md:p-8 mx-auto">
-    <div v-if="this.$store.getters.getRole === 'ADMIN'"></div>
+  <section class="container p-4 mx-auto md:p-8">
+    <AdminIndex v-if="this.$store.getters.getRole === 'ADMIN'" />
     <StudentIndex v-if="this.$store.getters.getRole === 'STUDENT'" />
     <div v-if="this.$store.getters.getRole === 'INSTRUCTOR'">
       INSTRUCTOR home
@@ -11,6 +11,7 @@
 <script>
 import Shell from "../components/layout/Shell";
 import StudentIndex from "../components/student/StudentIndex.vue";
+import AdminIndex from "../components/admin/AdminIndex.vue";
 import { AUTH_LOGOUT } from "../store/actions/auth";
 
 export default {
@@ -22,6 +23,7 @@ export default {
   components: {
     Shell,
     StudentIndex,
+    AdminIndex,
   },
   created() {
     if (this.$store.getters.getRole === "") {
