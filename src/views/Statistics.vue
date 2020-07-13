754d1d72d37baaa7902dbb27b2ad53441610e59b
Carlos <carlosaldaravi@gmail.com>
2020-07-13 18:11:16 +0200

    [ADD] statistics view

---
+++ src/views/Statistics.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container p-4 mx-auto md:p-8"></section>
+</template>
+
+<script>
+import StudentIndex from "../components/student/StudentIndex.vue";
+import AdminIndex from "../components/admin/AdminIndex.vue";
+import { AUTH_LOGOUT } from "../store/actions/auth";
+
+export default {
+  data() {
+    return {
+      sidebarOpen: true,
+    };
+  },
+  components: {},
+  created() {
+    if (this.$store.getters.getRole === "") {
+      this.$store.dispatch(AUTH_LOGOUT);
+    }
+  },
+};
+</script>
