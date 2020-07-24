610dd2728b7533c7b5057f75f6061fde1d0d4ab9
Carlos <carlosaldaravi@gmail.com>
2020-07-24 11:15:01 +0200

    [ADD] tailwind config font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   theme: {
+    fontSize: {
+      xxs: ".695rem",
+      xs: ".75rem",
+      s: ".825rem",
+      sm: ".875rem",
+      tiny: ".925rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+    },
     inset: {
       "1/2": "50%",
       "0": "0",
