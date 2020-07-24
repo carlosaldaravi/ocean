17436c42b736def9e19263969f919e40102f0fea
Carlos <carlosaldaravi@gmail.com>
2020-07-24 11:15:59 +0200

    [FIX] check id modal

--- src/mixins/UI.js
+++ src/mixins/UI.js
@@ -6,12 +6,16 @@ export const UI = {
   },
   methods: {
     openModal(id) {
-      document.body.style.overflow = "hidden";
-      document.getElementById(id).classList.remove("hidden-modal");
+      if (id) {
+        document.body.style.overflow = "hidden";
+        document.getElementById(id).classList.remove("hidden-modal");
+      }
     },
     closeModal(id) {
-      document.body.style.overflow = "auto";
-      document.getElementById(id).classList.add("hidden-modal");
+      if (id) {
+        document.body.style.overflow = "auto";
+        document.getElementById(id).classList.add("hidden-modal");
+      }
     },
   },
 };
