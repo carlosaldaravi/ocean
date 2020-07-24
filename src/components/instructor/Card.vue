d5390d9fdd20f9d1c21638a3f0578be92c49ddb7
Carlos <carlosaldaravi@gmail.com>
2020-07-24 21:15:28 +0200

    [UP] instructor list finished

--- src/components/instructor/Card.vue
+++ src/components/instructor/Card.vue
@@ -1,34 +1,110 @@
 <template>
-  <section>
+  <div>
     <div v-if="!instructor">No hay instructor</div>
-    <div v-else class="w-full max-w-sm lg:max-w-full lg:flex">
-      <div
-        class="flex-none h-48 overflow-hidden text-center bg-cover rounded-t lg:h-auto lg:w-48 lg:rounded-t-none lg:rounded-l"
-        :style="style"
-        title="Woman holding a mug"
-      ></div>
-      <div
-        class="flex flex-col justify-between p-4 leading-normal bg-white border-b border-l border-r border-gray-400 rounded-b lg:border-l-0 lg:border-t lg:border-gray-400 lg:rounded-b-none lg:rounded-r"
-      >
-        <div class="mb-8">
-          <div class="mb-2 text-xl font-bold text-gray-900">{{instructor.email}}</div>
-          <p
-            class="text-base text-gray-700"
-          >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+    <div
+      v-else
+      @click="moreCard = !moreCard"
+      class="relative flex flex-col h-auto mt-3 bg-opacity-75 rounded shadow sm:flex-row"
+    >
+      <div class="w-full h-full p-4">
+        <!-- target  -->
+        <div class="flex justify-between">
+          <div class="flex">
+            <img
+              v-if="getInstructorGender(instructor) == 'MALE'"
+              class="object-cover object-top rounded-full w-9 h-9"
+              src="../../assets/images/avatar/will.jpg"
+              alt
+            />
+            <img
+              v-if="getInstructorGender(instructor) == 'FEMALE'"
+              class="object-cover object-top rounded-full w-9 h-9"
+              src="../../assets/images/avatar/kaya.jpg"
+              alt
+            />
+            <div class="flex items-center ml-4">
+              <div class="text-xl">
+                {{ instructor.details.firstname }}
+                {{ instructor.details.lastname }}
+              </div>
+            </div>
+          </div>
+          <span
+            v-if="instructor.status == 'ACTIVE'"
+            class="inline-block w-4 h-4 overflow-hidden text-green-600"
+          >
+            <svg fill="currentColor" viewBox="0 0 24 24">
+              <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+            </svg>
+          </span>
+          <span
+            v-if="instructor.status == 'INACTIVE'"
+            class="inline-block w-4 h-4 overflow-hidden text-red-600"
+          >
+            <svg fill="currentColor" viewBox="0 0 24 24">
+              <path
+                d="M0 10a10 10 0 1120 0 10 10 0 01-20 0zm16-5L5 16A8 8 0 0016 5zm-1-1A8 8 0 004 15L15 4z"
+              />
+            </svg>
+          </span>
+        </div>
+
+        <div v-if="moreCard" class="mt-4">
+          <div class="flex justify-between">
+            <!-- instructor -->
+            <div class="flex justify-between mt-2 align-baseline">
+              <div class="flex items-center">
+                <span class="inline-block w-4 h-4 overflow-hidden">
+                  <svg fill="currentColor" viewBox="0 0 24 24">
+                    <path
+                      d="M20 18v1a1 1 0 01-1 1h-2A17 17 0 010 3V1a1 1 0 011-1h4a1 1 0 011 1v4L5 7 3 9l8 8 2-2 2-1h4a1 1 0 011 1v3z"
+                    />
+                  </svg>
+                </span>
+                <div>Teléfono:</div>
+                <div class="ml-3">{{ instructor.details.phone }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-if="instructor" class="flex justify-center -mb-3">
+          <button>
+            <img
+              v-if="!moreCard"
+              class="w-4 h-4 text-red-500"
+              src="../../assets/icons/cheveron-down.svg"
+              alt
+            />
+            <img
+              v-else
+              class="w-4 h-4 text-red-500"
+              src="../../assets/icons/cheveron-up.svg"
+              alt
+            />
+          </button>
         </div>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
+import { API } from "../../classes/api";
 export default {
   props: {
-    instructor: { type: Object, required: true }
+    instructor: { type: Object, required: true },
   },
-  computed: {}
+  data() {
+    return {
+      api: new API(),
+      moreCard: false,
+    };
+  },
+  computed: {},
+  methods: {
+    getInstructorGender(instructor) {
+      return instructor.details.gender;
+    },
+  },
 };
 </script>
-
-<style>
-</style>
--- src/components/instructor/List.vue
+++ src/components/instructor/List.vue
@@ -1,28 +1,28 @@
 <template>
   <section>
-    <div v-if="instructors.length == 0">No hay instructors disponibles</div>
+    <div v-if="instructors.length == 0" class="mt-4">
+      No hay instructors disponibles
+    </div>
     <div v-else>
-      <card v-for="instructor in instructors" :key="instructor.id" :instructor="instructor"></card>
+      <card
+        v-for="instructor in instructors"
+        :key="instructor.id"
+        :instructor="instructor"
+      ></card>
     </div>
   </section>
 </template>
 
 <script>
-import { API } from "../../classes/api";
 import Card from "./Card.vue";
 
 export default {
   name: "List", // used to devtools, nothing more
-  data() {
-    return {
-      api: new API()
-    };
-  },
   props: {
-    instructors: { type: Array, required: true }
+    instructors: { type: Array, required: true },
   },
   components: {
-    Card
-  }
+    Card,
+  },
 };
 </script>
