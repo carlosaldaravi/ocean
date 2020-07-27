f10c2cb70453a15fbe31254add36e8cd5aaaa233
Carlos <carlosaldaravi@gmail.com>
2020-07-28 01:38:51 +0200

    [ADD] vue config file

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    themeColor: "#17a2b8",
+    msTileColor: "#000000",
+    iconPaths: {
+      msTileImage: "img/icons/mstile-150x150.png",
+    },
+  },
+};
