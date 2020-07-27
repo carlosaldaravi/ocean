565dc568c13250b6738d0d72b9dde6b5fc67e871
Carlos <carlosaldaravi@gmail.com>
2020-07-27 03:58:16 +0200

    [UP] tailwind config screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   theme: {
+    screens: {
+      xs: "440px",
+      // => @media (min-width: 440px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
     fontSize: {
       xxs: ".695rem",
       xs: ".75rem",
