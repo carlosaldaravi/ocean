394a39485b2946172efba4fee63b29fb746191c7
Carlos <carlosaldaravi@gmail.com>
2020-07-27 03:59:29 +0200

    [UP] component text color

--- src/components/forms/Checkbox.vue
+++ src/components/forms/Checkbox.vue
@@ -4,7 +4,7 @@
       <input
         v-model="isChecked"
         type="checkbox"
-        class="w-4 h-4 text-indigo-600 transition duration-150 ease-in-out form-checkbox"
+        class="w-4 h-4 transition duration-150 ease-in-out text-primary-300 form-checkbox"
         :disabled="disabled"
       />
     </div>
@@ -19,31 +19,31 @@ export default {
   // Vue magic so we can use v-model on the parent instead of the :value binding and the @data emitter
   model: {
     prop: "checked",
-    event: "isChecked"
+    event: "isChecked",
   },
   props: {
     label: {
       type: String,
-      required: true
+      required: true,
     },
     checked: {
       type: [Boolean, Number, String],
-      required: true
+      required: true,
     },
     disabled: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
   watch: {
     isChecked() {
       this.$emit("isChecked", this.isChecked);
-    }
+    },
   },
   data() {
     return {
-      isChecked: this.checked
+      isChecked: this.checked,
     };
-  }
+  },
 };
 </script>
