ccd3321e02fb6aaa00955ea307fa529d0e00d853
Carlos <carlosaldaravi@gmail.com>
2020-08-12 21:02:18 +0200

    [REF] new component for students in course

--- src/components/course/CourseCard.vue
+++ src/components/course/CourseCard.vue
@@ -1,29 +1,13 @@
 <template>
   <div>
     <div v-if="this.$store.getters.getRole === 'INSTRUCTOR'">
-      <Modal
-        v-for="courseStudent of course.courseStudents"
-        :key="
-          'course_' +
-            courseStudent.courseId +
-            '_student_' +
-            courseStudent.studentId
-        "
-        :id="
-          'modal_validate_targets_course_' +
-            courseStudent.courseId +
-            '_student_' +
-            courseStudent.studentId
-        "
-      >
-        <div v-if="studentToValidate">
+      <Modal :id="'course_' + course.id">
+        <div v-if="courseStudent">
           <div class="mt-1 text-center">
             <h3
               class="text-xl font-medium leading-6 text-primary-100"
               id="modal-headline"
-            >
-              Validar objetivos
-            </h3>
+            >Validar objetivos</h3>
           </div>
           <div>
             <div class="col-span-6 sm:col-span-3">
@@ -31,14 +15,10 @@
                 <label
                   for="alumnoSelected"
                   class="block text-lg font-medium leading-5 text-primary-200"
-                  >Alumno
-                </label>
-                <div
-                  class="block text-lg font-medium leading-5 text-primary-200"
-                  v-if="studentToValidate"
-                >
-                  {{ studentToValidate.details.firstname }}
-                  {{ studentToValidate.details.lastname }}
+                >Alumno</label>
+                <div class="block text-lg font-medium leading-5 text-primary-200">
+                  {{ courseStudent.student.details.firstname }}
+                  {{ courseStudent.student.details.lastname }}
                 </div>
               </div>
               <div class="flex justify-between">
@@ -49,7 +29,7 @@
                 v-for="studentTarget of studentTargets"
                 :key="
                   'student_' +
-                    studentToValidate.id +
+                    courseStudent.studentId +
                     '_target_' +
                     studentTarget.targetId
                 "
@@ -79,24 +59,18 @@
                   @click="validateTargets()"
                   type="button"
                   class="inline-flex justify-center w-full px-4 py-2 text-base font-medium leading-6 text-white transition duration-150 ease-in-out border border-transparent rounded-md shadow-sm bg-primary-200 hover:bg-primary-300 focus:outline-none focus:border-red-700 focus:shadow-outline-red sm:text-sm sm:leading-5"
-                >
-                  Confirmar
-                </button>
+                >Confirmar</button>
               </span>
-              <span
-                class="flex w-full mt-3 rounded-md shadow-sm sm:mt-0 sm:w-auto"
-              >
+              <span class="flex w-full mt-3 rounded-md shadow-sm sm:mt-0 sm:w-auto">
                 <button
                   @click="
                     closeModal(
-                      `modal_validate_targets_course_${course.id}_student_${studentToValidate.id}`
+                      `course_${course.id}`
                     )
                   "
                   type="button"
                   class="inline-flex justify-center w-full px-4 py-2 text-base font-medium leading-6 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue sm:text-sm sm:leading-5"
-                >
-                  Cancelar
-                </button>
+                >Cancelar</button>
               </span>
             </div>
           </div>
@@ -118,12 +92,7 @@
           src="../../assets/images/iniciacion.jpg"
           alt
         />
-        <img
-          v-else
-          class="object-cover w-full h-48"
-          src="../../assets/images/kitesurf.png"
-          alt
-        />
+        <img v-else class="object-cover w-full h-48" src="../../assets/images/kitesurf.png" alt />
       </div>
 
       <div class="w-full h-full p-4 xl:w-2/3">
@@ -131,9 +100,7 @@
         <div class="flex justify-between">
           <div class="flex items-center">
             <img src="../../assets/images/calendario.svg" class="w-6 h-6" alt />
-            <div class="pl-2">
-              {{ getDate(course.calendar[0].start) }}
-            </div>
+            <div class="pl-2">{{ getDate(course.calendar[0].start) }}</div>
           </div>
 
           <div class="flex items-center sm:mr-4">
@@ -176,84 +143,35 @@
             <div class="ml-3">
               <p
                 class="text-sm font-medium leading-5 text-gray-700 group-hover:text-gray-900"
-              >
-                {{ course.courseInstructors[0].instructor.details.firstname }}
-              </p>
+              >{{ course.courseInstructors[0].instructor.details.firstname }}</p>
               <p
                 v-if="
                   course.courseInstructors[0].instructor.details.gender ==
                     'FEMALE'
                 "
                 class="text-xs font-medium leading-4 text-gray-500 transition duration-150 ease-in-out group-hover:text-gray-700 group-focus:underline"
-              >
-                Instructora
-              </p>
+              >Instructora</p>
               <p
                 v-else
                 class="text-xs font-medium leading-4 text-gray-500 transition duration-150 ease-in-out group-hover:text-gray-700 group-focus:underline"
-              >
-                Instructor
-              </p>
+              >Instructor</p>
             </div>
           </div>
         </div>
-        <div v-if="moreCard" class="mt-4">
-          <div class="">
+        <div v-show="moreCard" class="mt-4">
+          <div class>
             <!-- alumnos -->
             <div class="flex justify-between mt-2 align-baseline">
               <div>Alumnos</div>
-              <div v-if="this.$store.getters.getRole === 'INSTRUCTOR'">
-                Objetivos
-              </div>
+              <div>Objetivos</div>
             </div>
-            <div
-              v-for="courseStudent of course.courseStudents"
-              :key="courseStudent.studentId"
-              class="flex justify-between mt-2 align-baseline"
-            >
-              <div class="flex items-center">
-                <div>
-                  <img
-                    class="object-cover object-top rounded-full w-9 h-9"
-                    src="../../assets/images/avatar/will.jpg"
-                    alt
-                  />
-                </div>
-                <div class="ml-3">
-                  <p class="text-sm font-medium leading-5">
-                    {{ courseStudent.student.details.firstname }}
-                    {{ courseStudent.student.details.lastname }}
-                  </p>
-                </div>
-              </div>
-              <!-- targets -->
-              <div class="flex justify-between align-baseline">
-                <div
-                  v-if="$store.getters.getRole === 'ALUMNO'"
-                  class="flex items-center"
-                >
-                  <img
-                    class="w-5 h-5"
-                    src="../../assets/images/logros.svg"
-                    alt
-                  />
-                  <div class="pl-1 mb-1 text-sm">
-                    {{ getTargetsValidated(courseStudent.student) }}
-                  </div>
-                </div>
-                <div v-if="$store.getters.getRole === 'INSTRUCTOR'">
-                  <button
-                    @click="
-                      moreCard = !moreCard;
-                      openModalToValidate(courseStudent.student);
-                    "
-                    type="button"
-                    class="inline-flex justify-center w-full px-1 transition duration-150 ease-in-out border border-transparent rounded-md shadow-sm text-xxs text-primary-400 bg-primary-200 hover:bg-primary-300 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo sm:text-sm sm:leading-5"
-                  >
-                    Validar
-                  </button>
-                </div>
-              </div>
+            <div v-for="courseStudent of course.courseStudents" :key="courseStudent.studentId">
+              <CourseStudent
+                :courseStudent="courseStudent"
+                :courseId="course.id"
+                @isMoreCard="moreCard = false"
+                @setCourseStudent="setCourseStudent"
+              ></CourseStudent>
             </div>
           </div>
         </div>
@@ -265,12 +183,7 @@
               src="../../assets/icons/cheveron-down.svg"
               alt
             />
-            <img
-              v-else
-              class="w-4 h-4 text-red-500"
-              src="../../assets/icons/cheveron-up.svg"
-              alt
-            />
+            <img v-else class="w-4 h-4 text-red-500" src="../../assets/icons/cheveron-up.svg" alt />
           </button>
         </div>
       </div>
@@ -285,18 +198,24 @@ import { UI } from "../../mixins/UI";
 import moment from "moment";
 import { StudentTarget } from "../../classes/student-target";
 import { API } from "../../classes/api";
+import CourseStudent from "./CourseStudent.vue";
 
 export default {
   components: {
     Modal,
     StudentTarget,
+    CourseStudent,
+  },
+  computed: {
+    curso: function () {
+      return this.curso;
+    },
   },
   data() {
     return {
       api: new API(),
       moreCard: false,
-      studentToValidate: null,
-      targetsSelected: [],
+      courseStudent: null,
       studentTargets: [],
     };
   },
@@ -320,35 +239,13 @@ export default {
     getTime(date) {
       return moment(date).format("HH:mm");
     },
-    getTargetsValidated(student) {
-      let totalTargets = 0;
-      let validatedTargets = 0;
-      let studentTargets = student.studentTargets;
-      if (studentTargets) {
-        totalTargets = studentTargets.length;
-        studentTargets.forEach((st) => {
-          if (st.target.levelId == this.course.level.id && st.validatedBy) {
-            // this.targetsSelected.push(st.target);
-            validatedTargets++;
-          }
-        });
-      }
-
-      return validatedTargets + "/" + totalTargets;
-    },
-    openModalToValidate(student) {
-      this.studentToValidate = student;
-      student.studentTargets.forEach((studentTarget) => {
+    setCourseStudent(event) {
+      this.courseStudent = event;
+      this.studentTargets = [];
+      event.student.studentTargets.forEach((studentTarget) => {
         this.studentTargets.push(new StudentTarget(studentTarget));
       });
-      if (this.studentToValidate) {
-        UI.methods.openModal(
-          "modal_validate_targets_course_" +
-            this.course.id +
-            "_student_" +
-            student.id
-        );
-      }
+      this.openModal(`course_${this.course.id}`);
     },
     markAll(event) {
       this.studentTargets.forEach((st) => (st.checked = event.target.checked));
@@ -358,9 +255,19 @@ export default {
       this.studentTargets.forEach((st) => {
         if (st.checked) {
           targets.push({
-            studentId: this.studentToValidate.id,
+            studentId: this.courseStudent.studentId,
             targetId: st.targetId,
             feedback: "",
+            validatedBy: $store.getters.getUserId,
+            validatedDate: new Date(),
+          });
+        } else {
+          targets.push({
+            studentId: this.courseStudent.studentId,
+            targetId: st.targetId,
+            feedback: "",
+            validatedBy: null,
+            validatedDate: null,
           });
         }
       });
@@ -368,6 +275,20 @@ export default {
       console.log(res);
       if (res.data.data) {
         // change student to student updated
+        let studentId = this.courseStudent.studentId;
+        let auxCourse = this.course;
+        let studentIndex = -1;
+        this.closeModal(`course_${this.course.id}`);
+        this.course.courseStudents.forEach(function (item, i) {
+          if (item.studentId == studentId) {
+            studentIndex = i;
+            auxCourse.courseStudents[i] = res.data.data;
+          }
+        });
+        this.course = auxCourse;
+        console.log(this.course.courseStudents[studentIndex]);
+
+        // this.getTargetsValidated(this.course.courseStudents[studentIndex]);
       }
     },
   },
---
+++ src/components/course/CourseStudent.vue
@@ -0,0 +1,85 @@
+<template>
+  <div v-if="courseStudent">
+    <div class="flex justify-between mt-2 align-baseline">
+      <div class="flex items-center">
+        <div>
+          <img
+            class="object-cover object-top rounded-full w-9 h-9"
+            src="../../assets/images/avatar/will.jpg"
+            alt
+          />
+        </div>
+        <div class="ml-3">
+          <p class="text-sm font-medium leading-5">
+            {{ courseStudent.student.details.firstname }}
+            {{ courseStudent.student.details.lastname }}
+          </p>
+        </div>
+      </div>
+      <!-- targets -->
+      <div class="flex justify-between align-baseline">
+        <div class="flex items-center">
+          <img class="w-5 h-5" src="../../assets/images/logros.svg" alt />
+          <div class="pl-1 mb-1 text-sm">{{ validatedTargets }} / {{ totalTargets }}</div>
+        </div>
+        <div v-if="$store.getters.getRole === 'INSTRUCTOR'" class="mt-1 ml-2">
+          <button
+            @click="openModal(courseStudent.student)"
+            type="button"
+            class="inline-flex justify-center w-full px-1 transition duration-150 ease-in-out border border-transparent rounded-md shadow-sm text-xxs text-primary-400 bg-primary-200 hover:bg-primary-300 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo sm:text-sm sm:leading-5"
+          >Validar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  computed: {
+    totalTargets: function () {
+      return this.courseStudent.student.studentTargets.length;
+    },
+    validatedTargets: function () {
+      let numValidated = 0;
+      this.courseStudent.student.studentTargets.forEach((st) => {
+        if (st.validatedBy) {
+          numValidated++;
+        }
+      });
+      return numValidated;
+    },
+  },
+  data() {
+    return {};
+  },
+  props: {
+    courseStudent: { type: Object, required: true },
+    courseId: { type: Number, required: true },
+  },
+  methods: {
+    getTargetsValidated(student) {
+      let totalTargets = 0;
+      let validatedTargets = 0;
+      let studentTargets = student.studentTargets;
+      if (studentTargets) {
+        totalTargets = studentTargets.length;
+        studentTargets.forEach((st) => {
+          if (st.validatedBy) {
+            validatedTargets++;
+          }
+        });
+      }
+
+      return validatedTargets + "/" + totalTargets;
+    },
+    openModal(student) {
+      this.$emit("isMoreCard");
+      this.$emit("setCourseStudent", this.courseStudent);
+    },
+  },
+};
+</script>
+
+<style></style>
