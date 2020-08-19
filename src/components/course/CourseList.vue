c875652b10da39da62f1db69075cd1d378856a2e
Carlos <carlosaldaravi@gmail.com>
2020-08-19 11:13:38 +0200

    [UP] text

--- src/components/course/CourseList.vue
+++ src/components/course/CourseList.vue
@@ -1,17 +1,11 @@
 <template>
   <section>
-    <div
-      v-if="courses.length > 0"
-      class="grid grid-cols-1 gap-4 xl:grid-cols-2"
-    >
+    <div v-if="courses.length > 0" class="grid grid-cols-1 gap-4 xl:grid-cols-2">
       <div v-for="course of courses" :key="course.id">
         <CourseCard :course="course" />
       </div>
     </div>
-    <div v-else>
-      No tienes ningún curso pendiente, ve a calendario para ver los cursos
-      disponibles
-    </div>
+    <div v-else>No hay cursos que mostrar</div>
   </section>
 </template>
 
