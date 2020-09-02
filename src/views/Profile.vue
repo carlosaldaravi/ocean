ecd8fae7ed1508074dd4b1ec0bf58bec99c6e087
Carlos <carlosaldaravi@gmail.com>
2020-09-02 02:05:36 +0200

    [UP] delete sport modal css

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -153,32 +153,32 @@
                         <Modal
                           :id="'confirm_remove_sport_' + userSport.sportId"
                         >
-                          <slot>
-                            <div
-                              class="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10"
-                            >
-                              <svg
-                                class="w-6 h-6 text-red-600"
-                                stroke="currentColor"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                              >
-                                <path
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-width="2"
-                                  d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                                />
-                              </svg>
-                            </div>
-                          </slot>
                           <div>
-                            <h3
-                              class="text-lg font-medium leading-6 text-gray-900"
-                              id="modal-headline"
-                            >
-                              Eliminar
-                            </h3>
+                            <div class="flex items-center mx-auto">
+                              <span
+                                class="flex items-center justify-center flex-shrink-0 w-12 h-12 bg-red-100 rounded-full sm:h-10 sm:w-10"
+                              >
+                                <svg
+                                  class="w-6 h-6 text-red-600"
+                                  stroke="currentColor"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                >
+                                  <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                                  />
+                                </svg>
+                              </span>
+                              <h3
+                                class="ml-4 text-lg font-medium leading-6 text-gray-900"
+                                id="modal-headline"
+                              >
+                                Eliminar
+                              </h3>
+                            </div>
                             <div class="mt-2">
                               <p class="text-sm leading-5 text-gray-700">
                                 ¿Estás seguro que quieres eliminar de tus
